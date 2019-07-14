dcc22f6698817455d636efa366516b34e99b7170
Robin Whitworth <rwhitworth@tripadvisor.com>
2019-07-14 16:43:38 -0400

    added index.html & forward to it from /

--- initial/src/main/java/hello/HelloController.java
+++ initial/src/main/java/hello/HelloController.java
@@ -1,14 +1,16 @@
 package hello;
 
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class HelloController {
     
     @RequestMapping("/")
-    public String index() {
-        return "Greetings from Spring Boot!";
+    public ModelAndView index(ModelMap model) {
+        return new ModelAndView("forward:/index.html");
     }
 
     @RequestMapping("/echo")
