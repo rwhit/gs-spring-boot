3a2e0f768c217e5001a96ce23e1936634b0aac30
Robin Whitworth <rwhitworth@tripadvisor.com>
2019-07-16 01:00:32 -0400

    connect react & redux - basic articles form/list working

--- initial/ui/src/index.js
+++ initial/ui/src/index.js
@@ -1,21 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Route, BrowserRouter as Router } from 'react-router-dom'
-//import './index.css';
+import { Provider } from 'react-redux';
+import store from './store/index';
 import Game from './ticTacToe';
 import Index from './mainIndex.js';
+import ReduxApp from './components/reduxApp.jsx';
 
-const routing = (
-    <Router>
-      <div>
-        <Route exact path="/" component={Index} />
-        <Route path="/ticTacToe" component={Game} />
-      </div>
-    </Router>
+const routing = ({store}) => (
+  <Provider store={store}>
+      <Router>
+          <div>
+              <Route exact path="/" component={Index} />
+              <Route path="/ticTacToe" component={Game} />
+              <Route path="/ReduxApp" component={ReduxApp} />
+          </div>
+      </Router>
+  </Provider>
 )
 
 ReactDOM.render(
-  routing,
+  routing({store}),
   document.getElementById('root')
 );
 
--- initial/ui/src/mainIndex.js
+++ initial/ui/src/mainIndex.js
@@ -1,23 +1,24 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-// for playing in console only
-import index from './reactStore'
 
 class Index extends React.Component {
   render() {
     return (
       <div>
-        <h1>Main Index</h1>
-        <div>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/ticTacToe">Tic Tac Toe</Link>
-            </li>
-          </ul>
-        </div>
+          <h1>Main Index</h1>
+          <div>
+              <ul>
+                  <li>
+                      <Link to="/">Home</Link>
+                  </li>
+                  <li>
+                      <Link to="/ticTacToe">Tic Tac Toe</Link>
+                  </li>
+                  <li>
+                      <Link to="/ReduxApp">Redux Tutorial</Link>
+                  </li>
+              </ul>
+          </div>
       </div>
     )
   }
