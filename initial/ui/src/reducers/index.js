b123931bf2e665c651666b7b563208348d466289
Robin Whitworth <rwhitworth@tripadvisor.com>
2019-07-28 13:34:12 -0400

    change to switch

--- initial/ui/src/reducers/index.js
+++ initial/ui/src/reducers/index.js
@@ -10,59 +10,53 @@ const initialState = {
 
 function rootReducer(state = initialState, action) {
   console.log('rootReducer(' + JSON.stringify(initialState) + ', ' + JSON.stringify(action) + ')');
-  // TODO switch
-  if (action.type === ADD_ARTICLE) {
-    return Object.assign({}, state, {
-      fetching: true,
-      pendingArticle: action.payload
-    });
+  switch (action.type) {
+    case ADD_ARTICLE:
+      return Object.assign({}, state, {
+        fetching: true,
+        pendingArticle: action.payload
+      });
+    case ARTICLE_POSTED:
+      // TODO update id
+      return Object.assign({}, state, {
+        fetching: false,
+        articles: state.articles.concat(state.pendingArticle),
+        pendingArticle: null
+      });
+    case ARTICLES_LOADED:
+      return Object.assign({}, state, {
+        articles: state.articles.concat(action.payload)
+      });
+    case DATA_LOADED:
+      return Object.assign({}, state, {
+        remoteArticles: state.remoteArticles.concat(action.payload)
+      });
+    case FOUND_BAD_WORD:
+      return Object.assign({}, state, {
+        message: "One of the words in the title in not allowed",
+        messageType: "Warning"
+      });
+    case INFO_MESSAGE:
+      return Object.assign({}, state, {
+        message: action.payload.message,
+        messageType: action.payload.title ? action.payload.title : "Info"
+      });
+    case ERROR_MESSAGE:
+      return Object.assign({}, state, {
+        message: action.payload,
+        messageType: "Error",
+        fetching: false
+      });
+    case CLEAR_MESSAGE:
+      const cleared = Object.assign({}, state, {
+        message: null,
+        messageType: "Info"
+      });
+      console.log('cleared state: ' + cleared);
+      return cleared;
+    default:
+      return state;
   }
-  if (action.type === ARTICLE_POSTED) {
-    // TODO update id
-    return Object.assign({}, state, {
-      fetching: false,
-      articles: state.articles.concat(state.pendingArticle),
-      pendingArticle: null
-    });
-  }
-  if (action.type === ARTICLES_LOADED) {
-    return Object.assign({}, state, {
-      articles: state.articles.concat(action.payload)
-    });
-  }
-  if (action.type === DATA_LOADED) {
-    return Object.assign({}, state, {
-      remoteArticles: state.remoteArticles.concat(action.payload)
-    });
-  }
-  if (action.type === FOUND_BAD_WORD) {
-    return Object.assign({}, state, {
-      message: "One of the words in the title in not allowed",
-      messageType: "Warning"
-    });
-  }
-  if (action.type === INFO_MESSAGE) {
-    return Object.assign({}, state, {
-      message: action.payload.message,
-      messageType: action.payload.title ? action.payload.title : "Info"
-    });
-  }
-  if (action.type === ERROR_MESSAGE) {
-    return Object.assign({}, state, {
-      message: action.payload,
-      messageType: "Error",
-      fetching: false
-    });
-  }
-  if (action.type === CLEAR_MESSAGE) {
-    const cleared = Object.assign({}, state, {
-      message: null,
-      messageType: "Info"
-    });
-    console.log('cleared state: ' + cleared);
-    return cleared;
-  }
-  return state;
 }
 
 export default rootReducer;
